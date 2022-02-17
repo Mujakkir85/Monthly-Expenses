07a87d82f0c478340fe4291340db502ab5ba1958
Mujakkir85 <mujakkirlucse@gmail.com>
2022-02-17 16:03:20 +0600

    show error msg

--- JS/main.js
+++ JS/main.js
@@ -1,24 +1,35 @@
 
-function getIputField(inputField) {
-    return parseFloat(document.getElementById(inputField).value);
+function getIputField(inputField, erroMessage) {
+
+    const inputValue = parseFloat(document.getElementById(inputField).value);
+    //console.log(!isNaN(inputValue))
+    if (isNaN(inputValue)) {
+        erroMessage.style.display = 'block';
+    }
+    else {
+        return inputValue;
+    }
 }
 
 document.getElementById('calculate').addEventListener('click', function () {
 
-    const incomeField = getIputField('income-field');
-    const foodField = getIputField('food-field');
-    const rentField = getIputField('rent-field');
-    const clothesField = getIputField('clothes-field');
+    const erroMessage = document.getElementById('error-msg')
+    const erroMessage2 = document.getElementById('error-msg-two')
+
+    const incomeField = getIputField('income-field', erroMessage);
+    const foodField = getIputField('food-field', erroMessage);
+    const rentField = getIputField('rent-field', erroMessage);
+    const clothesField = getIputField('clothes-field', erroMessage);
 
     const totalExpenses = foodField + rentField + clothesField;
     const balance = incomeField - totalExpenses;
 
-    if (totalExpenses < balance) {
+    if (totalExpenses < incomeField) {
         document.getElementById('total-expenses').innerText = totalExpenses;
         document.getElementById('total-balance').innerText = balance;
     }
-    else {
-
+    else if (!isNaN(totalExpenses) && !isNaN(incomeField)) {
+        erroMessage2.style.display = 'block';
     }
 
 })
